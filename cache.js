cf76efdf401d7095b47a87b89a5f66160e8716fa
aarotoiv <aaro.toivonen@gmail.com>
2020-02-18 00:06:19 +0200

    update

--- cache.js
+++ cache.js
@@ -1,31 +1,34 @@
 const NodeCache = require('node-cache');
 
+let self;
+
 class Cache {
     constructor() {
         this.cache = new NodeCache({stdTTL: 43200});
+        self = this;
     }
-    seekExistingPlan = (req, res, next) => {
-        if(this && this.cache) {
-            const existingData = this.cache.get( req.params.class );
+    seekExistingPlan(req, res, next) {
+        if(self && self.cache) {
+            const existingData = self.cache.get( req.params.class );
             req.existingData = existingData;
-        }
+        } 
         next();
     }
-    savePlan = (classId, data) => {
-        if(this && this.cache) {
-            this.cache.set(classId, data);
+    savePlan(classId, data) {
+        if(self && self.cache) {
+            self.cache.set(classId, data);
         }
     }
-    seekExistingMenu = (req, res, next) => {
-        if(this && this.cache) {
-            const existingMenu = this.cache.get( 'menu' );
+    seekExistingMenu(req, res, next) {
+        if(self && self.cache) {
+            const existingMenu = self.cache.get( 'menu' );
             req.existingMenu = existingMenu;
         }
         next();
     }
-    saveMenu = (menu) => {
-        if(this && this.cache) {
-            this.cache.set('menu', menu, 7200);
+    saveMenu(menu) {
+        if(self && self.cache) {
+            self.cache.set('menu', menu, 7200);
         }
     }
 }
