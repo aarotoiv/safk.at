bcc8bb2bf8d5eeefc1da0d6a4cc646b05f1d6911
aarotoiv <aaro.toivonen@gmail.com>
2020-02-27 06:10:36 +0200

    added some static files for mobile client

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const device = require('express-device');
-
+const cors = require('cors');
 const app = express();
 
+app.use(cors());
 app.use(device.capture());
 app.use(morgan('dev'));
 app.use(bodyParser.json());
@@ -14,6 +15,8 @@ app.set('view engine', 'ejs');
 
 const ruoke = require('./routes');
 app.use('/', ruoke);
+
+app.use('/files',  express.static(__dirname + '/files'));
 //port = 80 in prod
 //5000 or something in dev
 const port = process.env.PORT || 5000;
