efc6290a846484071362caa4af83c36f1a17fd7b
aarotoiv <aaro.toivonen@gmail.com>
2020-05-19 22:43:19 +0300

    Remove comments

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const device = require('express-device');
 const cors = require('cors');
 const app = express();
 const socket = require('socket.io');
-/*
+
 app.use(cors());
 app.use(device.capture());
 app.use(morgan('dev'));
@@ -22,9 +22,9 @@ app.use('/files',  express.static(__dirname + '/files'));
 //5000 or something in dev
 const port = process.env.PORT || 5000;
 
-const server = app.listen(port, () => console.log("RUNNING IN PORT", port));*/
+const server = app.listen(port, () => console.log("RUNNING IN PORT", port));
 
-const io = socket(5000);
+const io = socket(server);
 
 let mainController;
 let slaveController;
